2554d553727427f68fa473fdd46e7cbda88a6902
Hannah Milam <hannah.milam16@gmail.com>
2022-02-23 21:37:42 -0700

    rendering user info on front end

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,7 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -46,6 +47,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
